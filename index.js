3cdefa2e1a3a84f480e69898d850eec4ada96157
Felipe-Severo <felipisevero@gmail.com>
2025-02-10 21:34:20 -0300

    Implementing Route for Creation and Listing News

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ import dotenv from 'dotenv';
 import userRoute from './src/routes/user.route.js';
 import carRoute from './src/routes/car.route.js';
 import authRoute from './src/routes/auth.route.js';
+import newsRoute from './src/routes/news.route.js';
 
 dotenv.config();
 
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
 connectDatabase();
 app.use(express.json());
 app.use('/user', userRoute);
 app.use('/car', carRoute);
 app.use('/auth', authRoute);
+app.use('/news', newsRoute);
 
 app.listen(port, () => {
     console.log(`App está rodando na porta ${port}`);
---
+++ src/controllers/news.controller.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import { createService, findAllService } from '../services/news.service.js';
+
+const create = async (req, res) => {
+    try {
+        const { title, text, banner } = req.body;
+
+        if (!title || !text || !banner) {
+            return res.status(400).send({ message: "Submit all fields for create news" });
+        }
+
+        // const userId = "67a69b53a5cee76cf25ecfa0";
+
+        // if (!mongoose.Types.ObjectId.isValid(userId)) {
+        //     return res.status(400).send({ message: "Invalid user ID" });
+        // }
+
+        await createService({
+            title,
+            text,
+            banner,
+            user: { _id: "67a69b53a5cee76cf25ecfa0" },
+            //user: new mongoose.Types.ObjectId(userId),
+        });
+
+        res.sendStatus(201);
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send({ message: "Validation error", errors: error.errors });
+        }
+
+        res.status(400).send({ message: error.message });
+    }
+}
+const findAll = async (req, res) => {
+    const news = await findAllService();
+    if (news.length === 0) {
+        res.status(400).send({ message: "There are no news" });
+    }
+
+    res.send(news);
+}
+
+export { create, findAll };
